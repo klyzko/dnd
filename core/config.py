78498684d3a821c3c3b21b863360f16f0ae03100
Zahar <43670879+klyzko@users.noreply.github.com>
2026-03-23 04:31:35 +0300

    Setting config

--- core/config.py
+++ core/config.py
@@ -1,5 +1,6 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
+import os
 
 
 class Settings(BaseSettings):
@@ -16,9 +17,21 @@ class Settings(BaseSettings):
 
     # Настройки DeepSeek (добавим позже)
     DEEPSEEK_API_KEY: Optional[str] = None
+    #Settings_db
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
 
-    class Config:
-        env_file = ".env"
+    # DATABASE_SQLITE = 'sqlite+aiosqlite:///data/db.sqlite3'
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
+    )
+
+    def get_db_url(self):
+        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+                f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
 
 
 settings = Settings()
