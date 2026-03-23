39fb8f5269650c99fadb92bd5c9a8650b8cf16f9
Zahar <43670879+klyzko@users.noreply.github.com>
2026-03-23 04:32:11 +0300

    db Base

--- core/database.py
+++ core/database.py
@@ -1,21 +1,16 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.core.config import settings
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from config import settings
 
-# Создание движка (один на приложение)
-engine = create_engine(
-    settings.DATABASE_URL,
-    pool_size=20,  # для PostgreSQL
-    max_overflow=10 # логировать SQL запросы (только для разработки)
-)
 
-# Создание фабрики сессий
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+DATABASE_URL = settings.get_db_url()
 
-# Базовый класс для моделей
-Base = declarative_base()
+# Создаем асинхронный движок для работы с базой данных
+engine = create_async_engine(url=DATABASE_URL)
+# Создаем фабрику сессий для взаимодействия с базой данных
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+# Базовый класс для всех моделей
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True  # Класс абстрактный, чтобы не создавать отдельную таблицу для него
