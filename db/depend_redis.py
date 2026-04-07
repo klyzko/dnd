3c00c12c6b0c0cbb7145cc8e35661c703018cd90
Zahar <43670879+klyzko@users.noreply.github.com>
2026-04-08 02:09:23 +0300

    update redis client

---
+++ db/depend_redis.py
@@ -0,0 +1,34 @@
+import redis.asyncio as redis
+from contextlib import asynccontextmanager
+from core.config import settings
+from fastapi import FastAPI
+
+
+redis_client: redis.Redis = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Запускается при старте и остановке приложения"""
+    global redis_client
+
+    # Открываем соединение при старте
+    url=settings.get_redis_url()
+    redis_client = await redis.from_url(
+        url,
+        decode_responses=True
+    )
+    #print("✅ Redis connected")
+
+    yield
+
+    # Закрываем соединение при остановке
+    if redis_client:
+        await redis_client.close()
+        await redis_client.connection_pool.disconnect()
+        #print("❌ Redis disconnected")
+
+
+async def get_redis() -> redis.Redis:
+    """Возвращает Redis клиент (один на всё приложение)"""
+    return redis_client
